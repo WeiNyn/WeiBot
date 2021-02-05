1f77ac401b6865378f2f1bd18d95d1e6558fa71e
WeiNyn <nguyenhuy17798@gmail.com>
2021-02-05 09:15:26 +0700

    add restart action to default config

--- actions/defined_actions.py
+++ actions/defined_actions.py
@@ -1,7 +1,7 @@
 from actions.actions import BaseActionClass
 from typing import Dict, List, Any
 
-from parsers.event import ButtonEvent
+from parsers.event import ButtonEvent, SetSlotEvent, TextEvent, EventOutput
 
 
 class DefaultAction(BaseActionClass):
@@ -39,7 +39,7 @@ class DefaultAction(BaseActionClass):
             RegulatedAnnualLeave="Regulated Annual Leave"
         )
     
-    def __call__(self, intent: Dict[str, Any], entities: List[Dict[str, Any]], slots: Dict[str, Any]):
+    def __call__(self, intent: Dict[str, Any], entities: List[Dict[str, Any]], slots: Dict[str, Any]) -> EventOutput:
         """
         Based on the ranking of intents, suggest the most likely intent to user
         :param intent: dict(name, intent_ranking, priority) - current intent of user message
@@ -57,10 +57,53 @@ class DefaultAction(BaseActionClass):
             button=[dict(
                 title=x[0],
                 trigger_intent=x[1]
-            ) for x in intent_ranking[:5]]
+            ) for x in intent_ranking[:5]] + [dict(
+                title="Restart",
+                trigger_intent="restart"
+            )]
         )
 
         button_event = ButtonEvent(button, self.entities_list, self.intents_list, self.slots_list)
 
         return button_event(intent, entities, slots)
+
+
+class RestartAction(BaseActionClass):
+    """
+    Action that handle "restart" event.
+    """
+    def __init__(self, entities_list: List[str], intents_list: List[str], slots_list: List[str]):
+        super().__init__(entities_list, intents_list, slots_list)
+
+    def name(self):
+        return "restart"
+
+    def __call__(self, intent: Dict[str, Any], entities: List[Dict[str, Any]], slots: Dict[str, Any]) -> EventOutput:
+        """
+        Reset the conversation
+        :param intent: dict(name, intent_ranking, priority) - current intent of user message
+        :param entities: list(dict(entity_name, text, ...)) - current entities of user message
+        :param slots: dict(slot_name) - current slots of user message
+        :return: EventOutput - the output of ButtonEvent of current conversation.
+        """
+        set_slot = dict()
+
+        for key, value in slots.items():
+            if value is not None:
+                set_slot[key] = None
+
+        set_slot_event = SetSlotEvent(set_slot, self.entities_list, self.intents_list, self.slots_list)(intent, entities, slots)
+
+        text = ["Conversation has been restarted"]
+
+        text_event = TextEvent(text, self.entities_list, self.intents_list, self.slots_list)(intent, entities, slots)
+
+        event = set_slot_event
+        event.append(text_event)
+
+        print(event.__dict__)
+
+        return event
+
+
 
